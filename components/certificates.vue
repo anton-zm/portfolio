a290146c848e6851c7b17e52b618d1bdeccd0b4e
AntonZ <antonzm34@gmail.com>
2021-01-16 13:26:36 +0300

    certif

--- components/certificates.vue
+++ components/certificates.vue
@@ -1,8 +1,19 @@
 <template>
-  <section class="certificates">
+  <section class="certificates" id="certificates">
     <content-box class="certificates__content">
       <ContentTitle :title="title" />
-      <div class="certificates__container"></div>
+      <div class="certificates__container">
+        <img
+          :src="cert1"
+          alt="YandexPractikum Certificate"
+          class="certificates__item"
+        />
+        <img
+          :src="cert2"
+          alt="YandexPraktikum Certificate"
+          class="certificates__item"
+        />
+      </div>
     </content-box>
   </section>
 </template>
@@ -29,14 +40,53 @@ export default {
         return this.titleEng
       }
     },
+    cert1() {
+      if (this.ru) {
+        return this.cert1Ru
+      } else {
+        return this.cert1Eng
+      }
+    },
+    cert2() {
+      if (this.ru) {
+        return this.cert2Ru
+      } else {
+        return this.cert2Eng
+      }
+    },
   },
   data() {
     return {
       titleRu: 'Сертификаты',
       titleEng: 'Certificates',
+      cert1Ru: '/cert1_ru.jpg',
+      cert1Eng: '/cert1_eng.jpg',
+      cert2Ru: '/cert2_ru.jpg',
+      cert2Eng: '/cert2_eng.jpg',
     }
   },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.certificates {
+  width: 100%;
+  padding: 40px 0;
+}
+.certificates__content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.certificates__container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-top: 32px;
+}
+.certificates__item {
+  width: calc(100% / 4);
+  margin-right: 24px;
+  cursor: pointer;
+}
+</style>
